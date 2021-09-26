b0c40cd1a8b34b8602c46c10d421e144f4bd9d0c
Athiya Rastogi <47875071+athiyarastogi@users.noreply.github.com>
2021-09-26 15:11:26 -0400

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -12,3 +12,15 @@ class Solution(object):
                 if nums[j]==result:
                     return (i,j)
             
+            
+            
+ OR
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                result=0
+                result+=nums[i]+nums[j]
+                if result==target:
+                    return(i, j)
